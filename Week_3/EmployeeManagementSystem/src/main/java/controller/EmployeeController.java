c912b89423fe364e36c60dce4bfd590921eb8488
riyamahantyyy <riyamahanty04@gmail.com>
2024-08-12 02:15:04 +0530

    WEEK3_Solution q4

--- Week_3/EmployeeManagementSystem/src/main/java/controller/EmployeeController.java
+++ Week_3/EmployeeManagementSystem/src/main/java/controller/EmployeeController.java
@@ -1,27 +1,51 @@
 package controller;
 
+
 import Model.Employee;
-import Model.Department;
 import service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/employees/{email}")
-    public Employee getEmployeeByEmail(@PathVariable String email) {
-        return employeeService.getEmployeeByEmail(email);
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
-    @GetMapping("/departments/{name}")
-    public Department getDepartmentByName(@PathVariable String name) {
-        return employeeService.getDepartmentByName(name);
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id)
+                .map(employee -> new ResponseEntity<>(employee, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    // Add other endpoints for CRUD operations here
+    @GetMapping
+    public ResponseEntity<Iterable<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee)
+                .map(updatedEmployee -> new ResponseEntity<>(updatedEmployee, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        if (employeeService.deleteEmployee(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- Week_3/EmployeeManagementSystem/src/main/java/service/EmployeeService.java
+++ Week_3/EmployeeManagementSystem/src/main/java/service/EmployeeService.java
@@ -1,11 +1,11 @@
 package service;
 
 import Model.Employee;
-import Model.Department;
 import Repository.EmployeeRepository;
-import Repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -13,17 +13,31 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Autowired
-    private DepartmentRepository departmentRepository;
-
-    // Example methods to use repositories
-    public Employee getEmployeeByEmail(String email) {
-        return employeeRepository.findByEmail(email);
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
     }
 
-    public Department getDepartmentByName(String name) {
-        return departmentRepository.findByName(name);
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
     }
 
-    // Add other CRUD operations here
+    public Iterable<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> updateEmployee(Long id, Employee employee) {
+        if (employeeRepository.existsById(id)) {
+            employee.setId(id);
+            return Optional.of(employeeRepository.save(employee));
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteEmployee(Long id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
