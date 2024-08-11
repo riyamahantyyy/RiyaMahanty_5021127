414711a323841db94eacf386a0cd9c26563a83cd
riyamahantyyy <riyamahanty04@gmail.com>
2024-08-12 03:20:02 +0530

    WEEK3_Solution q8

--- Week_3/EmployeeManagementSystem/src/main/java/Repository/EmployeeRepository.java
+++ Week_3/EmployeeManagementSystem/src/main/java/Repository/EmployeeRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
-
+import Projection.EmployeeProjection;
+import Projection.EmployeeSummary;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	// Custom query using @Query annotation
@@ -26,4 +27,16 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     
  // Method to find employees by department with pagination
     Page<Employee> findByDepartmentId(Long departmentId, Pageable pageable);
+
+    @Query("SELECT e FROM Employee e WHERE e.email = :email")
+    EmployeeProjection findByEmailProjection(@Param("email") String email);
+
+    @Query("SELECT e FROM Employee e")
+    List<EmployeeProjection> findAllEmployeeProjections();
+    
+    @Query("SELECT new com.employee.EmployeeManagementSystem2.projection.EmployeeSummary(e.id, e.name, e.email) FROM Employee e WHERE e.email = :email")
+    EmployeeSummary findByEmailSummary(@Param("email") String email);
+
+    @Query("SELECT new com.employee.EmployeeManagementSystem2.projection.EmployeeSummary(e.id, e.name, e.email) FROM Employee e")
+    List<EmployeeSummary> findAllEmployeeSummaries();
 }
