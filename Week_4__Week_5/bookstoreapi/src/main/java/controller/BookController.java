52c0b679f05f2251f13aa4ddcfa5501789033216
riyamahantyyy <riyamahanty04@gmail.com>
2024-08-26 08:26:36 +0530

    WEEK4_5_Solution q9

--- Week_4__Week_5/bookstoreapi/src/main/java/controller/BookController.java
+++ Week_4__Week_5/bookstoreapi/src/main/java/controller/BookController.java
@@ -2,13 +2,19 @@ package controller;
 
 import model.Book;
 import repository.BookRepository;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/books")
@@ -18,32 +24,57 @@ public class BookController {
     private BookRepository bookRepository;
 
     @GetMapping
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public List<EntityModel<Book>> getAllBooks() {
+        List<Book> books = bookRepository.findAll();
+
+        return books.stream()
+                .map(book -> EntityModel.of(book,
+                        linkTo(methodOn(BookController.class).getBookById(book.getId())).withSelfRel(),
+                        linkTo(methodOn(BookController.class).getAllBooks()).withRel("books")))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<Book>> getBookById(@PathVariable Long id) {
         Optional<Book> book = bookRepository.findById(id);
-        return book.map(ResponseEntity::ok)
-                   .orElseGet(() -> ResponseEntity.notFound().build());
+
+        if (book.isPresent()) {
+            EntityModel<Book> resource = EntityModel.of(book.get(),
+                    linkTo(methodOn(BookController.class).getBookById(id)).withSelfRel(),
+                    linkTo(methodOn(BookController.class).getAllBooks()).withRel("books"));
+            return ResponseEntity.ok(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Book> createBook(@Valid @RequestBody Book book) {
-        return ResponseEntity.status(201).body(bookRepository.save(book));
+    public ResponseEntity<EntityModel<Book>> createBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        EntityModel<Book> resource = EntityModel.of(savedBook,
+                linkTo(methodOn(BookController.class).getBookById(savedBook.getId())).withSelfRel(),
+                linkTo(methodOn(BookController.class).getAllBooks()).withRel("books"));
+
+        return ResponseEntity.created(resource.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(resource);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long id, @Valid @RequestBody Book bookDetails) {
+    public ResponseEntity<EntityModel<Book>> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
         Optional<Book> book = bookRepository.findById(id);
+
         if (book.isPresent()) {
             Book updatedBook = book.get();
             updatedBook.setTitle(bookDetails.getTitle());
             updatedBook.setAuthor(bookDetails.getAuthor());
             updatedBook.setPrice(bookDetails.getPrice());
             updatedBook.setIsbn(bookDetails.getIsbn());
-            return ResponseEntity.ok(bookRepository.save(updatedBook));
+            bookRepository.save(updatedBook);
+
+            EntityModel<Book> resource = EntityModel.of(updatedBook,
+                    linkTo(methodOn(BookController.class).getBookById(updatedBook.getId())).withSelfRel(),
+                    linkTo(methodOn(BookController.class).getAllBooks()).withRel("books"));
+
+            return ResponseEntity.ok(resource);
         } else {
             return ResponseEntity.notFound().build();
         }
@@ -52,6 +83,7 @@ public class BookController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         Optional<Book> book = bookRepository.findById(id);
+
         if (book.isPresent()) {
             bookRepository.delete(book.get());
             return ResponseEntity.noContent().build();
--- Week_4__Week_5/bookstoreapi/src/main/java/controller/CustomerController.java
+++ Week_4__Week_5/bookstoreapi/src/main/java/controller/CustomerController.java
@@ -2,13 +2,18 @@ package controller;
 
 import model.Customer;
 import repository.CustomerRepository;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/customers")
@@ -18,31 +23,56 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+    public List<EntityModel<Customer>> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+
+        return customers.stream()
+                .map(customer -> EntityModel.of(customer,
+                        linkTo(methodOn(CustomerController.class).getCustomerById(customer.getId())).withSelfRel(),
+                        linkTo(methodOn(CustomerController.class).getAllCustomers()).withRel("customers")))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+    public ResponseEntity<EntityModel<Customer>> getCustomerById(@PathVariable Long id) {
         Optional<Customer> customer = customerRepository.findById(id);
-        return customer.map(ResponseEntity::ok)
-                       .orElseGet(() -> ResponseEntity.notFound().build());
+
+        if (customer.isPresent()) {
+            EntityModel<Customer> resource = EntityModel.of(customer.get(),
+                    linkTo(methodOn(CustomerController.class).getCustomerById(id)).withSelfRel(),
+                    linkTo(methodOn(CustomerController.class).getAllCustomers()).withRel("customers"));
+            return ResponseEntity.ok(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer) {
-        return ResponseEntity.status(201).body(customerRepository.save(customer));
+    public ResponseEntity<EntityModel<Customer>> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        EntityModel<Customer> resource = EntityModel.of(savedCustomer,
+                linkTo(methodOn(CustomerController.class).getCustomerById(savedCustomer.getId())).withSelfRel(),
+                linkTo(methodOn(CustomerController.class).getAllCustomers()).withRel("customers"));
+
+        return ResponseEntity.created(resource.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(resource);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @Valid @RequestBody Customer customerDetails) {
+    public ResponseEntity<EntityModel<Customer>> updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails) {
         Optional<Customer> customer = customerRepository.findById(id);
+
         if (customer.isPresent()) {
             Customer updatedCustomer = customer.get();
             updatedCustomer.setName(customerDetails.getName());
             updatedCustomer.setEmail(customerDetails.getEmail());
             updatedCustomer.setAddress(customerDetails.getAddress());
-            return ResponseEntity.ok(customerRepository.save(updatedCustomer));
+            customerRepository.save(updatedCustomer);
+
+            EntityModel<Customer> resource = EntityModel.of(updatedCustomer,
+                    linkTo(methodOn(CustomerController.class).getCustomerById(updatedCustomer.getId())).withSelfRel(),
+                    linkTo(methodOn(CustomerController.class).getAllCustomers()).withRel("customers"));
+
+            return ResponseEntity.ok(resource);
         } else {
             return ResponseEntity.notFound().build();
         }
@@ -51,6 +81,7 @@ public class CustomerController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
         Optional<Customer> customer = customerRepository.findById(id);
+
         if (customer.isPresent()) {
             customerRepository.delete(customer.get());
             return ResponseEntity.noContent().build();
